638231a0bbcbcf12adec5f5f9ea37f562fed151e
Florent Chiappelloni <fchiappe@fu-r1-p5.s19.be>
2025-08-15 08:11:20 +0200

    added C05 to retry

---
+++ retry/C05/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fchiappe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/14 18:31:39 by fchiappe          #+#    #+#             */
+/*   Updated: 2025/08/14 18:34:22 by fchiappe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = 1;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return 1;
+	while (power > 0)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
