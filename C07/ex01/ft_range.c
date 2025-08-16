cf8c9e559775d8eb6b637d1bdc20255b1985ce99
Florent Chiappelloni <fchiappe@fu-c2-p3.s19.be>
2025-08-16 15:29:33 +0200

    Added ex01

---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fchiappe <fchiappe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 15:23:30 by fchiappe          #+#    #+#             */
+/*   Updated: 2025/08/16 15:28:46 by fchiappe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int *ft_range(int min, int max)
+{
+    int *array;
+    int i;
+
+    array = (int *) malloc(sizeof(int) * ((max - min) + 1));
+    if (min >= max)
+    {
+        array = NULL;
+        return (0);
+    }
+    i = 0;
+    while (min < max)
+    {
+        array[i] = min;
+        i++;
+        min++;
+    }
+    return(array);
+}
