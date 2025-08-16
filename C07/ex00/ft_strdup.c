45709c6ed625fd65a7e9c06e6fa6e4a4025855db
Florent Chiappelloni <fchiappe@fu-c2-p3.s19.be>
2025-08-16 15:20:36 +0200

    added C07

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fchiappe <fchiappe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 14:38:26 by fchiappe          #+#    #+#             */
+/*   Updated: 2025/08/16 14:46:57 by fchiappe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char    *ft_strdup(char *src)
+{
+    char    *dest;
+    int i;
+    int lenght;
+
+    lenght = 0;
+    while (src[lenght])
+    lenght++;
+
+    dest = (char *) malloc(sizeof(char) * (lenght + 1));
+
+    if (dest == NULL)
+    return (NULL);
+    i = 0;
+    while (i < lenght)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
