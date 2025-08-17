7b74f204a9504d8e2c7298e3e974b959bd08c233
Newansama <newan.play@gmail.com>
2025-08-17 10:44:29 +0200

    Update ft_strjoin

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -3,18 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fchiappe <fchiappe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: florentchiappelloni <florentchiappellon    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 16:09:47 by fchiappe          #+#    #+#             */
-/*   Updated: 2025/08/16 16:12:34 by fchiappe         ###   ########.fr       */
+/*   Updated: 2025/08/16 18:11:30 by florentchia      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char    *ft_strjoin(int size, char **str, char *sep)
+int ft_strlen(char *str)
 {
-    int dest;
+    int i;
 
-    dest = (char *) malloc(sizeof(char) * (*str + 1));
+    i = 0;
+    while (str[i])
+    i++;
+    return (i);
+}
+
+char    *ft_strcat(char *dest, char *src)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while(dest[i] != '\0')
+    i++;
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int total_len(int size, char **strs, char *sep)
+{
+    int total;
+    int i;
+
+    total = 0;
+    i = 0;
+    while (i < size)
+    {
+        total += ft_strlen(strs[i]);
+        i++;
+    }
+    total += ft_strlen(sep) * (size - 1);
+    return (total);
+}
+
+char    *ft_strjoin(int size, char **strs, char *sep)
+{
+    int *ret;
+    int total;
+    int i;
+
+    if (size == 0)
+    {
+        ret = malloc(sizeof(char));
+        ret[0] = '\0';
+        return (ret);
+    }
+    total = total_len(size, strs, sep);
+    ret = malloc(sizeof(char) * (total + 1));
+    if (ret == NULL)
+    return (NULL);
+    i = 0;
+    ret[0] = '\0';
+    while (i < size)
+    {
+        ft_strcat(ret, strs[i]);
+        if (i < size - 1)
+        ft_strcat(ret, sep);
+        i++;
+    }
+    return (ret);
 }
