5af774ca9d9e90298b0c4df78d04dcdbfd2e1686
Florent Chiappelloni <fchiappe@fu-r2-p2.s19.be>
2025-08-20 15:43:17 +0200

    added ex04 C08

---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fchiappe <fchiappe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 15:39:01 by fchiappe          #+#    #+#             */
+/*   Updated: 2025/08/20 15:42:52 by fchiappe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+	int		lenght;
+
+	lenght = 0;
+	while (src[lenght])
+		lenght++;
+	dest = (char *)malloc(sizeof(char) * (lenght + 1));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (i < lenght)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str(int size, char *str, char *copy)
+{}
