09c7d7ac73c0b516e039082483af3399be0d5c9d
Newansama <newan.play@gmail.com>
2025-08-20 17:36:49 +0200

    Finished 2 last exercice from C08

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fchiappe <fchiappe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: florentchiappelloni <florentchiappellon    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 15:39:01 by fchiappe          #+#    #+#             */
-/*   Updated: 2025/08/20 15:42:52 by fchiappe         ###   ########.fr       */
+/*   Updated: 2025/08/20 17:29:48 by florentchia      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include "ft_stock_str.h"
 
 int	ft_strlen(char *str)
 {
@@ -44,5 +46,27 @@ char	*ft_strdup(char *src)
 	return (dest);
 }
 
-struct s_stock_str(int size, char *str, char *copy)
-{}
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	struct s_stock_str *arr;
+	int i;
+
+	arr = (t_stock_str *) malloc(sizeof(t_stock_str *) * (ac + 1));
+	i = 0;
+	if (arr == NULL)
+	return (NULL);
+	while (i < ac)
+	{
+		if (av[1] == NULL)
+		return (NULL);
+
+		arr[i].size = ft_strlen(av[i]);
+		arr[i].str = ft_strdup(av[i]);
+		arr[i].copy = ft_strdup(av[i]);
+		if (arr[i].str == NULL || arr[i].copy == NULL)
+		return (NULL);
+		i++;
+ 	}
+	arr[i].str = NULL;
+	return (arr);
+}
